699c481cee6b3aecf9a645e42c4bbabecb916f48
‘Shelby <sls1881@gmail.com>
2021-01-20 20:30:17 -0800

    step 6 and some of 7

--- cart/cart.js
+++ cart/cart.js
@@ -1,6 +1,18 @@
 import { cartItem } from './cart-data.js';
-import { shoes } from '../product/data';
+import { shoes } from '../product/data.js';
 
 import { renderTableRow } from './cart-render.js';
+import { findById } from '../utils.js';
+
+const tbody = document.querySelector('tbody');
+
+for (let item of cartItem) {
+    const shoe = findById(item.id, shoes);
+
+    const dom = renderTableRow(item, shoe);
+
+    tbody.append(dom);
+}
+
 
 
