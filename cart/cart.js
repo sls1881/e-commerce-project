bd8335abbc0f5bd7bc40ccab89341d70dbf04945
‘Shelby <sls1881@gmail.com>
2021-01-20 22:56:54 -0800

    linking and part of step 7

--- cart/cart.js
+++ cart/cart.js
@@ -2,7 +2,7 @@ import { cartItem } from './cart-data.js';
 import { shoes } from '../product/data.js';
 
 import { renderTableRow } from './cart-render.js';
-import { findById } from '../utils.js';
+import { calcOrderTotal, findById } from '../utils.js';
 
 const tbody = document.querySelector('tbody');
 
@@ -14,5 +14,15 @@ for (let item of cartItem) {
     tbody.append(dom);
 }
 
+const tr = document.createElement('tr');
 
+const tdEmpty1 = document.createElement('td');
+tr.append(tdEmpty1);
+
+const tdEmpty2 = document.createElement('td');
+tr.append(tdEmpty2);
+
+const tdTotal = document.createElement('td');
+tdTotal.textContent = `Cart total: $${calcOrderTotal}`;
+tr.append(tdTotal);
 
--- utils.js
+++ utils.js
@@ -14,27 +14,19 @@ export function calcItemTotal(item, shoe) {
     return item.quantity * shoe.price;
 }
 
+let total = 0;
+
 export function calcOrderTotal(cartItem, shoes) {
-    let total = 0;
 
     for (let item of cartItem) {
-
 
         const totalByItem = calcItemTotal(item, shoe);
 
         total = total + totalByItem;
 
-        const tr = document.createElement('tr');
+        return total;
 
-        const tdEmpty1 = document.createElement('td');
-        tr.append(tdEmpty1);
-
-        const tdEmpty2 = document.createElement('td');
-        tr.append(tdEmpty2);
-
-        const tdTotal = document.createElement('td');
-        tdTotal.textContent = `Cart total: $${total}`;
-        tr.append(tdTotal);
     }
-
 }
+
+
