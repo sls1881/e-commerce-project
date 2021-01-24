b964557d3121fbbeb7c7cef4cea5fcaf96321285
‘Shelby <sls1881@gmail.com>
2021-01-24 12:38:05 -0800

    created form

---
+++ admin/admin-utils.js
@@ -0,0 +1,10 @@
+import { shoes } from '../product/render-shoe.js';
+
+const PRODUCT = 'PRODUCT';
+
+export function getInventory() {
+    //Check local storage to see if products are in inventory
+
+
+
+}
